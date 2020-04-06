095f454ec1e120c03258f465c845673a326d4513
Brent F <feuerbacherb@gmail.com>
2020-04-06 12:48:52 -0500

    added eventhandling to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var buttonE1 = document.querySelector("#save-task");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+/*
+buttonE1.addEventListener("click", function() {
+   var listItemE1 = document.createElement("li");
+   listItemE1.className = "task-item";
+   listItemE1.textContent = "This is a new task.";
+   tasksToDoE1.appendChild(listItemE1);
+});
+*/
+
+
+var createTaskHandler = function() {
+   var listItemE1 = document.createElement("li");
+   listItemE1.className = "task-item";
+   listItemE1.textContent = "This is a new task.";
+   tasksToDoE1.appendChild(listItemE1);
+}
+
+buttonE1.addEventListener("click", createTaskHandler);
