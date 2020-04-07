5a300ef9a21a65cc353a6f0bf4f42a8fab29bc19
Brent F <feuerbacherb@gmail.com>
2020-04-07 12:43:03 -0500

    saved script.js with changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,6 +141,20 @@ var taskButtonHandler = function(event) {
 var deleteTask = function(taskId) {
    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
    taskSelected.remove();
+
+   // create new array to hold updated list of tasks
+   var updatedTaskArr = [];
+
+   // loop through current tasks
+   for (var i = 0; i < tasks.length; i++) {
+      // if tasks[i].id doesn't match the value of the taskId, let's keep that task and push it into the new array
+      if (tasks[i].id !== parseInt(taskId)) {
+         updatedTaskArr.push(tasks[i]);
+      }
+   }
+
+   // reassign tasks array to be the same as updatedTaskArr
+   tasks = updatedTaskArr
 };
 
 var editTask = function(taskId) {
@@ -171,6 +185,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
    taskSelected.querySelector("h3.task-name").textContent = taskName;
    taskSelected.querySelector("span.task-type").textContent = taskType;
 
+   // loop through tasks array and task object with new content
+   for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id === parseInt(taskId)) {
+         tasks[i].name = taskName;
+         tasks[i].type - taskType;
+      }
+   }
+
    alert("Task Updated!");
 
    formE1.removeAttribute("data-task-id");
@@ -195,6 +217,13 @@ var taskStatusChangeHandler = function(event) {
    }
    else if (statusValue === "completed") {
       tasksCompletedE1.appendChild(taskSelected);
+   }
+
+   // update task's in tasks array
+   for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id === parseInt(taskId)) {
+         tasks[i].status = statusValue;
+      }
    }
 }
 
